f4c099f5fad4920295ba620f2fdf7c9017a9e3f4
Memo <memo@hermes.dev>
2026-04-26 09:50:33 +0200

    autocode: [P1] [CRITICAL] Electron main process + preload script (closes #73)

---
+++ electron/preload.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Type declarations for the Electron preload API.
+ * Augments the global Window interface so TypeScript knows about `window.api`.
+ */
+
+export interface ElectronAPI {
+  readFileBuffer: (filePath: string) => Promise<Uint8Array>
+  getAppVersion: () => Promise<string>
+}
+
+declare global {
+  interface Window {
+    api: ElectronAPI
+  }
+}
